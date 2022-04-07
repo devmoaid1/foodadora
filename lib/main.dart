4ebba9b154e4d24a1b921c1df5f91f6bad87dc7d
Yasser Ehab <yehab2000@gmail.com>
2022-04-07 22:10:16 +0200

    setup localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:foodadora/app/app.locator.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:foodadora/foodadora/foodadora.dart';
-
 import 'app/utilites/custom_modals.dart';
+import 'package:foodadora/app/app.locator.dart';
+import 'package:foodadora/translate_preferences.dart';
 
 void main() async {
+  LocalizationDelegate delegate = await LocalizationDelegate.create(
+    fallbackLocale: 'en',
+    supportedLocales: ['en', 'tr'],
+    preferences: TranslatePreferences(),
+    basePath: 'assets/i18n/',
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   setupLocator();
   setupDialogUi();
 
-  runApp(DevicePreview(builder: (context) => const FoodadoraApp()));
+  runApp(
+    LocalizedApp(
+      delegate,
+      DevicePreview(
+        builder: (context) => const FoodadoraApp(),
+      ),
+    ),
+  );
 }
