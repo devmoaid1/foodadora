e4aeb8b84fa51cc62543ce4a63aa84bf1fb8fc33
Yasser Ehab <yehab2000@gmail.com>
2022-04-19 23:42:41 +0200

    Create Adaptive pressable button widget

---
+++ lib/ui/widgets/pressable.dart
@@ -0,0 +1,32 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/material.dart';
+import 'package:touchable_opacity/touchable_opacity.dart';
+
+// Adaptive Pressable Widget
+
+class Pressable extends StatelessWidget {
+  final Widget child;
+  final Function() onPressed;
+  final BorderRadius? borderRadius;
+  const Pressable({
+    Key? key,
+    required this.child,
+    required this.onPressed,
+    this.borderRadius,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? TouchableOpacity(
+            child: child,
+            onTap: onPressed,
+          )
+        : InkWell(
+            child: child,
+            onTap: onPressed,
+            borderRadius: borderRadius,
+          );
+  }
+}
