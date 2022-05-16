cf172d9dc334021c4f4deab75b58a9d80cfa2952
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2022-05-16 14:25:36 +0200

    Replace LocalizationDelegate hard coded attributes with constants

---
+++ lib/app/constants/localization_constants.dart
@@ -0,0 +1,3 @@
+const String fallbackLocale = 'en';
+const List<String> supportedLocales = ['en', 'tr'];
+const String localizationAssetsPath = 'assets/i18n/';
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:foodadora/app/constants/localization_constants.dart';
 import 'package:foodadora/foodadora/foodadora.dart';
 import 'app/utilites/custom_modals.dart';
 import 'package:foodadora/app/app.locator.dart';
@@ -10,10 +11,10 @@ import 'package:foodadora/translate_preferences.dart';
 
 void main() async {
   LocalizationDelegate delegate = await LocalizationDelegate.create(
-    fallbackLocale: 'en',
-    supportedLocales: ['en', 'tr'],
+    fallbackLocale: fallbackLocale,
+    supportedLocales: supportedLocales,
     preferences: TranslatePreferences(),
-    basePath: 'assets/i18n/',
+    basePath: localizationAssetsPath,
   );
 
   WidgetsFlutterBinding.ensureInitialized();
